fb7ede3628d122133d82a6b28adfac8b577b8314
hcankaynak <h.cankaynak@gmail.com>
2024-07-28 00:57:38 +0300

    gonic installed. basic web server created.

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// Start the server
 	fmt.Println("Hello world!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
