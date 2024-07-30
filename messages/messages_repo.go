f8b9fa45df0d939e3f4695a12c169fdf0e19a0be
hcankaynak <h.cankaynak@gmail.com>
2024-07-30 16:51:25 +0300

    find and update func added to message repo

--- messages/messages_repo.go
+++ messages/messages_repo.go
@@ -22,7 +22,7 @@ This function is used to find sent messages.
 */
 func (m *MessageRepository) FindSentMessages() ([]Message, error) {
 	var messages []Message
-	err := m.DB.Where("sending_status = ?", true).Find(&messages).Error
+	err := m.DB.Where("sent_status = ?", true).Find(&messages).Error
 	return messages, err
 }
 
@@ -30,9 +30,9 @@ func (m *MessageRepository) ConvertEntityToDTO(messages []Message) []MessageDTO 
 	var messageDTOs []MessageDTO
 	for _, message := range messages {
 		messageDTOs = append(messageDTOs, MessageDTO{
-			Content:       message.Content,
-			PhoneNumber:   message.PhoneNumber,
-			SendingStatus: message.SendingStatus,
+			Content:     message.Content,
+			PhoneNumber: message.PhoneNumber,
+			SentStatus:  message.SentStatus,
 		})
 	}
 	return messageDTOs
@@ -68,4 +68,34 @@ func GenerateMessagesFromDummyData() []Message {
 	}
 
 	return messages
+}
+
+/*
+FindOldestTwoMessagesThatNotSent function
+This function is used to find the oldest two messages that are not sent.
+*/
+func (m *MessageRepository) FindOldestTwoMessagesThatNotSent(excludedMessages []string) ([]Message, error) {
+	var messages []Message
+	req := m.DB.Where("sent_status = ?", false).Order("created_at ASC").Limit(2)
+	if len(excludedMessages) > 0 {
+		req.Where("id NOT IN ?", excludedMessages)
+	}
+	err := req.Find(&messages).Error
+	if err != nil {
+		log.Fatalf("Error executing query: %v", err)
+		return nil, err
+	}
+
+	return messages, nil
+}
+
+/*
+SetMessageAsSent function
+This function is used to set a message as sent.
+*/
+func (m *MessageRepository) SetMessageAsSent(message Message) {
+	result := m.DB.Model(&message).Update("sent_status", true)
+	if result.Error != nil {
+		log.Fatalf("Failed to update message: %v", result.Error)
+	}
 }
